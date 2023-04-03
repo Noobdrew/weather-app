072ac93ba66727a187779537c620068d7339fd1b
Ivan Marinski <noobdrew23@gmail.com>
2023-04-03 14:33:34 +0300

    webpage design complete

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 
+const searchInput = document.querySelector('.search')
+console.log(searchInput.value)
+
 const apiKey = 'http://api.weatherapi.com/v1/forecast.json?key=511350788bca43a4b07120356232903&q=japan&days=3&aqi=no&alerts=no'
 
 async function getWeather() {
 
     const responce = await fetch(apiKey, { mode: 'cors' })
-     
+
 
     const data = await responce.json()
     console.log(data)
 }
 
-//getWeather()
+const weatherObj = getWeather()
+
+console.log(weatherObj)
