da6ca73d7dbbd87f4909fd979f119cfb385be9bc
Ivan Marinski <noobdrew23@gmail.com>
2023-04-05 13:46:50 +0300

    app done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let weatherLocation = 'london'
 let weatherObj = {}
+const metricButton = document.querySelector('.celsius')
+const imperialButton = document.querySelector('.farenheit')
 let apiKey = `http://api.weatherapi.com/v1/forecast.json?key=511350788bca43a4b07120356232903&q=${weatherLocation}&days=3&aqi=no&alerts=no`
 async function setApi() {
 
@@ -10,12 +12,39 @@ async function setApi() {
 
 }
 
+
 async function asignWeatherData() {
     weatherObj = await setApi()
     console.log(weatherObj)
     fillContentMetric()
+    if (imperialButton.classList.contains('active')) {
+        fillContentImperial()
+    }
+
 }
 asignWeatherData()
+
+function setUnits() {
+
+
+    metricButton.addEventListener('click', () => {
+        imperialButton.classList.remove('active')
+        if (!metricButton.classList.contains('active')) {
+            metricButton.classList.add('active')
+            fillContentMetric()
+        }
+    })
+
+    imperialButton.addEventListener('click', () => {
+        metricButton.classList.remove('active')
+        if (!imperialButton.classList.contains('active')) {
+            imperialButton.classList.add('active')
+            fillContentImperial()
+        }
+
+    })
+}
+setUnits()
 
 const inputForm = document.querySelector('.search-form')
 const searchInput = document.querySelector('.search')
@@ -110,6 +139,9 @@ function fillContentMetric() {
     const nextDay3 = document.querySelector('.day-3')
     const nextDay3Arr = nextDay3.children
 
+    const fieldTempTitle = document.querySelector('.field-temp')
+    fieldTempTitle.textContent = '°C'
+
     setNextDaysData(nextDay1Arr, 0)
     setNextDaysData(nextDay2Arr, 1)
     setNextDaysData(nextDay3Arr, 2)
@@ -123,13 +155,86 @@ function fillContentMetric() {
             } else if (element.classList.contains('date')) {
                 let nextDaysOfWeek = weekday[localDate.getDay() + index + 1]
                 element.textContent = nextDaysOfWeek
-            } else if(element.classList.contains('chance-rain')){
-                element.textContent = weatherObj.forecast.forecastday[index].hour[12].chance_of_rain+'%'
-            }   else if(element.classList.contains('max-wind-container')){
-               element.children[0].style.rotate =weatherObj.forecast.forecastday[index].hour[12].wind_degree+'deg'
-               element.children[1].textContent=Math.round((weatherObj.forecast.forecastday[index].hour[12].wind_kph/ 3.6 + Number.EPSILON) * 10) / 10
+            } else if (element.classList.contains('chance-rain')) {
+                element.textContent = weatherObj.forecast.forecastday[index].hour[12].chance_of_rain + '%'
+            } else if (element.classList.contains('max-wind-container')) {
+                element.children[0].style.rotate = weatherObj.forecast.forecastday[index].hour[12].wind_degree + 'deg'
+                element.children[1].textContent = Math.round((weatherObj.forecast.forecastday[index].hour[12].wind_kph / 3.6 + Number.EPSILON) * 10) / 10
             }
         });
     }
+
+}
+
+function fillContentImperial() {
+    //----------------------------------------get time data---------------------------------
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const dateTimeZone = new Date().toLocaleString('en-US', { timeZone: `${weatherObj.location.tz_id}` })
+    const localDate = new Date(dateTimeZone)
+    let dayOfWeek = weekday[localDate.getDay()];
+    //----------------------------------------get time data---------------------------------
+
+    //--------------------------------set temp in f------------------------
+    const currentWeatherIcon = document.querySelector('.weather-temp-img')
+    currentWeatherIcon.src = `${weatherObj.current.condition.icon}`
+
+    const currentWeatherText = document.querySelector('.weather-temp-text')
+    currentWeatherText.textContent = `${weatherObj.current.temp_f}°F`
+
+    const currentWeatherDescription = document.getElementById('weather-text')
+    currentWeatherDescription.textContent = `${weatherObj.current.condition.text}`
+
+    const feelsLike = document.querySelector('.feels-like')
+    feelsLike.textContent = `Feels like ${weatherObj.current.feelslike_f}°F`
+    //--------------------------------set temp in f------------------------
+
+    //--------------------------------set exta text imperial-------------------
+    const currentWindText = document.querySelector('.wind-text')
+    let windSpeedMph = weatherObj.current.wind_mph
+    currentWindText.textContent = `${windSpeedMph}mph`
+
+    const currentPressure = document.querySelector('.pressure-text')
+    currentPressure.textContent = `${weatherObj.current.pressure_in}`
+
+    const currentVisibility = document.querySelector('.visibility-text')
+    currentVisibility.textContent = `${weatherObj.current.vis_miles}mile`
+
+    const currentRainChance = document.querySelector('.precipitation-text')
+    currentRainChance.textContent = `${weatherObj.current.precip_in}in`
+    //--------------------------------set exta text imperial-------------------
+
+    //-------------------------------change next three days imperial------------
+    const nextDay1 = document.querySelector('.day-1')
+    const nextDay1Arr = nextDay1.children
+    const nextDay2 = document.querySelector('.day-2')
+    const nextDay2Arr = nextDay2.children
+    const nextDay3 = document.querySelector('.day-3')
+    const nextDay3Arr = nextDay3.children
+
+    const fieldTempTitle = document.querySelector('.field-temp')
+    fieldTempTitle.textContent = '°F'
+
+    setNextDaysData(nextDay1Arr, 0)
+    setNextDaysData(nextDay2Arr, 1)
+    setNextDaysData(nextDay3Arr, 2)
+
+
+    function setNextDaysData(DayArr, index) {
+        Array.from(DayArr).forEach(element => {
+            if (element.classList.contains('next-days-temp')) {
+                element.children[0].src = weatherObj.forecast.forecastday[index].day.condition.icon
+                element.children[1].textContent = weatherObj.forecast.forecastday[index].day.avgtemp_f
+            } else if (element.classList.contains('date')) {
+                let nextDaysOfWeek = weekday[localDate.getDay() + index + 1]
+                element.textContent = nextDaysOfWeek
+            } else if (element.classList.contains('chance-rain')) {
+                element.textContent = weatherObj.forecast.forecastday[index].hour[12].chance_of_rain + '%'
+            } else if (element.classList.contains('max-wind-container')) {
+                element.children[0].style.rotate = weatherObj.forecast.forecastday[index].hour[12].wind_degree + 'deg'
+                element.children[1].textContent = weatherObj.forecast.forecastday[index].hour[12].wind_mph
+            }
+        });
+    }
+
 
 }
